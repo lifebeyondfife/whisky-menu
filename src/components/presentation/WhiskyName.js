f08ecd9ec6fd13b950af3c55ba1e6e1f21c1af9c
Iain <iain@lifebeyondfife.com>
2020-02-09 15:06:22 +0000

    Add collapsable section purely within details component

--- src/components/presentation/WhiskyName.js
+++ src/components/presentation/WhiskyName.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './WhiskyName.css';
 
 function WhiskyName({whisky}) {
+  const showHideWhiskyDetails = () => {};
   return (
     <div className="container">
       <span className="left">{whisky.name}</span>
-      <span className="right">{whisky.location.region || whisky.location.country}</span>
+      <span className="right"><button onClick={showHideWhiskyDetails}>v</button></span>
       <span className="centre">&nbsp;</span>
     </div>
   );
